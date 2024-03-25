06f63c0cd3aeeb7cc1e9342bb52a3712cad37ef1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-25 09:57:31 -0300

    'first-spring' was added to the repository

--- first-spring/src/main/java/com/example/firstspring/HelloController.java
+++ first-spring/src/main/java/com/example/firstspring/HelloController.java
@@ -1,12 +1,16 @@
 package com.example.firstspring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
     @GetMapping(path = "/hello")
     public String sayHello(){
         return "Hello!";
+    }
+
+    @PostMapping(path = "/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello " + name + "!";
     }
 }
