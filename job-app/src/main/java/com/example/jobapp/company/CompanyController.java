36944ea4a3f7f9a71953069941570778dbcd1593
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-26 15:17:05 -0300

    'job-app' was updated to the repository

--- job-app/src/main/java/com/example/jobapp/company/CompanyController.java
+++ job-app/src/main/java/com/example/jobapp/company/CompanyController.java
@@ -1,6 +1,8 @@
 package com.example.jobapp.company;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,28 +14,41 @@ public class CompanyController {
     private CompanyService companyService;
 
     @GetMapping
-    public List<Company> getAll(){
-        return companyService.getAll();
+    public ResponseEntity<List<Company>> getAll(){
+        return new ResponseEntity<>(companyService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{companyId}")
-    public Company getCompanyById(@PathVariable(name = "companyId") int companyId){
-        return companyService.getCompanyById(companyId);
+    public ResponseEntity<Company> getCompanyById(@PathVariable(name = "companyId") int companyId){
+        Company company = companyService.getCompanyById(companyId);
+        return company!= null
+                ? new ResponseEntity<>(company, HttpStatus.OK)
+                : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public String createCompany(@RequestBody Company company){
-        companyService.createCompany(company);
-        return "OK!";
+    public ResponseEntity<String> createCompany(@RequestBody Company company){
+        try {
+            companyService.createCompany(company);
+            return new ResponseEntity<>("Company added successfully", HttpStatus.OK);
+        } catch (AssertionError ex){
+            return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping(path = "/{companyId}")
-    public Company updateCompany(@PathVariable(name = "companyId") int companyId, @RequestBody Company updatedCompany){
-        return companyService.updateCompany(companyId, updatedCompany);
+    public ResponseEntity<Company> updateCompany(@PathVariable(name = "companyId") int companyId, @RequestBody Company updatedCompany){
+        Company company = companyService.getCompanyById(companyId);
+        return company != null
+                ? new ResponseEntity<>(companyService.updateCompany(companyId, updatedCompany), HttpStatus.OK)
+                : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping(path = "/{companyId}")
-    public Company deleteCompanyById(@PathVariable int companyId){
-        return companyService.deleteCompanyById(companyId);
+    public ResponseEntity<Company> deleteCompanyById(@PathVariable int companyId){
+        Company company = companyService.getCompanyById(companyId);
+        return company != null
+                ? new ResponseEntity<>(companyService.deleteCompanyById(companyId), HttpStatus.OK)
+                : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
