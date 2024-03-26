99172dce208f278eaa021d5a22e244e3fbfe8c07
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-26 10:52:52 -0300

    'job-app' was updated to the repository

--- job-app/src/main/java/com/example/jobapp/job/JobController.java
+++ job-app/src/main/java/com/example/jobapp/job/JobController.java
@@ -8,36 +8,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/jobs/")
 public class JobController {
     @Autowired
     private JobService jobService;
 
-    @GetMapping(path = "/jobs/{jobId}")
+    //@RequestMapping(value = "/{jobId}", method = RequestMethod.GET)
+    @GetMapping(path = "/{jobId}")
     public ResponseEntity<Job> getJobById(@PathVariable(name = "jobId") int jobId){
         Job job = jobService.getJobById(jobId);
         return job.getJobId() != -1 ? new ResponseEntity<>(job, HttpStatus.OK) :
                 new ResponseEntity<>(job, HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping(path = "/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> getAll(){
         return ResponseEntity.ok(jobService.findAllJobs());
     }
 
-    @PostMapping(path = "/jobs")
+    @PostMapping
     public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
         return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
-    @DeleteMapping(path = "/jobs/{jobId}")
+    @DeleteMapping(path = "/{jobId}")
     public ResponseEntity<String> deleteJobById(@PathVariable(name = "jobId") int jobId){
         Job job = jobService.deleteJobById(jobId);
         return job.getJobId() != -1 ? new ResponseEntity<>("Job deleted successfully", HttpStatus.OK)
                 : new ResponseEntity<>("Job NOT Found", HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping(path = "/jobs/{jobId}")
+    @PutMapping(path = "/{jobId}")
     public ResponseEntity<String> updateJob(@PathVariable(name = "jobId") int jobId, @RequestBody Job updatedJob){
         return jobService.updateJob(jobId, updatedJob) ? new ResponseEntity<>("Jod updated successfully", HttpStatus.OK)
                 : new ResponseEntity<>("Job NOT Found", HttpStatus.NOT_FOUND);
