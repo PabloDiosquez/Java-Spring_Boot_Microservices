5e7c96f3c2967d03887e08a446a02dcd2654311a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-27 16:20:36 -0300

    'job-app' was updated to the repository

--- job-app/src/main/java/com/example/jobapp/review/impl/ReviewServiceImpl.java
+++ job-app/src/main/java/com/example/jobapp/review/impl/ReviewServiceImpl.java
@@ -1,35 +1,48 @@
 package com.example.jobapp.review.impl;
 
 import com.example.jobapp.review.Review;
+import com.example.jobapp.review.ReviewRepository;
 import com.example.jobapp.review.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
     @Override
     public List<Review> getAll() {
-        return null;
+        return reviewRepository.findAll();
     }
 
     @Override
     public Review getReviewById(int reviewId) {
-        return null;
+        return reviewRepository.findById(reviewId).orElse(null);
     }
 
     @Override
     public void createReview(Review review) {
-
+        reviewRepository.save(review);
     }
 
     @Override
     public Review deleteReviewById(int reviewId) {
-        return null;
+        Review review = getReviewById(reviewId);
+        reviewRepository.deleteById(reviewId);
+        return review;
     }
 
     @Override
     public Review updateReview(int reviewId, Review updatedReview) {
+        Optional<Review> optionalReview = reviewRepository.findById(reviewId);
+        if(optionalReview.isPresent()){
+            updatedReview.setReviewId(reviewId);
+            reviewRepository.save(updatedReview);
+            return updatedReview;
+        }
         return null;
     }
 }
